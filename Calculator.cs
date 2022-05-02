aa974cd1d01d218605ca4ace20daf599fb3e05b0
Ambili das v s <ambili.kan@gmail.com>
2022-05-02 20:57:25 +0530

    added calculator program

---
+++ Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    internal class Calculator
+    {
+        public void add()
+        {
+            int num = 10;
+            int num2 = 20;
+            int sum = num + num2;
+            Console.WriteLine("addition of {0} + {1} = {2}",num,num2,sum);
+        }
+
+        public void addition(int num1,int num2)
+        {
+            int sum = num1 + num2;
+            Console.WriteLine("Addition of {0} + {1} = {2}", num1, num2, sum);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+            Calculator ObjCalc=new Calculator();
+            Console.WriteLine("Enter 1st number");
+            int num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int num2=Convert.ToInt32(Console.ReadLine());
+            ObjCalc.addition(num1, num2);
+        }
+    }
+}
